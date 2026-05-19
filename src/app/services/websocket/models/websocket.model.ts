9a200fe6ef00f9718fe9c267d57e2ff1d22b3b25
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2026-05-19 16:29:15 +0200

    feat: refacto websocket for opti and implement socket status

---
+++ src/app/services/websocket/models/websocket.model.ts
@@ -0,0 +1,3 @@
+export interface IAppWebsocketInfo {
+  status: 'live' | 'off';
+}
--- src/app/services/websocket/websocket.service.ts
+++ src/app/services/websocket/websocket.service.ts
@@ -1,20 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Signal, signal } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
-import { WEBSOCKET_APP_EVENT, WEBSOCKET_APP_NAMESPACE } from '../../const/gateway.constant';
+import {
+  WEBSOCKET_APP_EVENT,
+  WEBSOCKET_APP_NAMESPACE,
+  WEBSOCKET_APP_ROOM,
+} from '../../const/gateway.constant';
+import { IAppWebsocketInfo } from './models/websocket.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WebsocketService {
   private socket: Socket;
+  private _websocketStatus = signal<IAppWebsocketInfo['status']>('off');
+
   constructor() {
     this.socket = io(`ws://localhost:3000/${WEBSOCKET_APP_NAMESPACE}`, {
-      reconnectionAttempts: 5,
+      reconnection: true,
+      reconnectionAttempts: Infinity,
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 5000,
       transports: ['websocket'],
+    });
+
+    this.socket.on('connect', () => {
+      this._websocketStatus.set('live');
+      this._joinChannel(WEBSOCKET_APP_ROOM);
+    });
+
+    this.socket.on('disconnect', () => {
+      this._websocketStatus.set('off');
+    });
+
+    this.socket.on('connect_error', (err) => {
+      this._websocketStatus.set('off');
     });
   }
 
-  public joinChannel(channelName: string): void {
+  public getWebsocketStatut(): Signal<IAppWebsocketInfo['status']> {
+    return this._websocketStatus.asReadonly();
+  }
+
+  private _joinChannel(channelName: string): void {
     this.socket.emit('join', channelName);
   }
 
