62f4ab9293547be75cefd9a78ce2cc1f226b8473
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2026-05-19 16:26:06 +0200

    feat: updating boostrap config for injecting google key

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { GoogleMapsLoaderService } from './services/google-map/google-map.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAppInitializer(() => {
+      const loader = inject(GoogleMapsLoaderService);
+      return loader.load();
+    }),
+  ],
 };
