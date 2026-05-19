236cc7126b644a44c255d77eb65d75e61c066737
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2026-05-19 16:28:31 +0200

    feat: updating icon logic to implement websocket status visual

--- src/app/components/info-card-icon/info-card-icon.ts
+++ src/app/components/info-card-icon/info-card-icon.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { IAppWebsocketInfo } from '../../services/websocket/models/websocket.model';
 
 @Component({
   selector: 'app-info-card-icon',
   imports: [],
   templateUrl: './info-card-icon.html',
 })
-export class InfoCardIcon {}
+export class InfoCardIcon {
+  public wsSatus = input<IAppWebsocketInfo['status']>('live');
+}
